8db5dc1a5793c462085bd9ff0aec45372ff25efd
Ny <89911360+Nyyu@users.noreply.github.com>
2022-02-12 17:48:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let play, back, forth, repeat, shuffle, options, returnPage, optionArea, cBool, counter;
+let play, back, forth, repeat, shuffle, options, returnPage, optionArea;
 
 // Variables
 
@@ -10,11 +10,9 @@ shuffle = document.querySelector(".fa-random");
 
 options = document.querySelector(".fa-ellipsis-h");
 optionArea = document.querySelector(".option-area");
-returnPage = document.querySelector(".fa-angle-left"); 
-counter = document.querySelector(".time");
+returnPage = document.querySelector(".fa-angle-left");
 
 // Functions - Methods
-
 returnPage.addEventListener("click", () => {
     return (location.href = "https://github.com/Nyyu/01-Test");
 });
@@ -25,15 +23,13 @@ play.addEventListener("click", () => {
         '<i class="fas fa-play-circle fa-6x" aria-hidden="true"></i>'
     ) {
         play.innerHTML = '<i class="fas fa-pause-circle fa-6x"></i>';
-        cBool = true;
     } else {
         play.innerHTML = '<i class="fas fa-play-circle fa-6x"></i>';
-        cBool = false;
     }
 });
 
-options.addEventListener("click", function() {
-    if(optionArea.classList.contains("visible")) {
+options.addEventListener("click", function () {
+    if (optionArea.classList.contains("visible")) {
         optionArea.classList.remove("visible");
     } else {
         optionArea.classList.add("visible");
